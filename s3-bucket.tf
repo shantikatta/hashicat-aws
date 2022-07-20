ca6f2d3adf236d1ba6ee105157c9d6d4b33347b0
shantikatta <shantikatta@gmail.com>
2022-07-20 19:29:06 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,3 @@ module "s3-bucket" {
   source  = "app.terraform.io/skatta-training/s3-bucket/aws"
   version = "2.8.0"
 }
-
-credentials "app.terraform.io" {
-    # valid user API token:
-    token = "pygls0OVWwH2Tg.atlasv1.FlrHDbYNVmNOCcW92hfOn81zH1Zz0YvUjCK8zELOnEyVXRtYMODTriotbIT0hOrdu5M"
-  }
